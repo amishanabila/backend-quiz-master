65c1dadab4fb501c7341f172becb6e4e1bc5cbe0
Amisha Nabila Wiguna <11231008@student.itk.ac.id>
2025-12-24 01:34:32 +0800

    up

--- src/config/db.js
+++ src/config/db.js
@@ -6,18 +6,16 @@ const dotenv = require('dotenv');
 
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT ,
-    user: process.env.DB_USER ,
-    password: process.env.DB_PASSWORD ,
-    database: process.env.DB_NAME ,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-     ssl: {
-    minVersion: 'TLSv1.2',
-    rejectUnauthorized: false 
-  }
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 10000 
 });
 
 console.log('Passed configuration to MySQL pool. Establishing connections...');
